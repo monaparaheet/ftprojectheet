49777db770a6390f29c5baf20814d2a98dcee53f
monaparaheet <monaparaheet2005@gmail.com>
2026-02-21 11:35:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# ftproject
+
+A new Flutter project.
+
+## Getting Started
+
+#descrition 
+
+This Flutter application integrates a public REST API (https://dummyjson.com/products/category/groceries) to fetch and display grocery products dynamically. The app parses JSON data and presents products in a responsive GridView layout.
+
+Each product card displays the product image, title, and price. The app uses FutureBuilder for asynchronous data handling and adapts the grid layout based on screen size for better responsiveness.
+
+The project demonstrates API integration, JSON parsing, responsive UI design, and dynamic data rendering in Flutter.
+
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Learn Flutter](https://docs.flutter.dev/get-started/learn-flutter)
+- [Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Flutter learning resources](https://docs.flutter.dev/reference/learning-resources)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,187 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GroceryScreen(),
+    );
+  }
+}
+
+class Product {
+  final int id;
+  final String title;
+  final String image;
+  final double price;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.price,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      image: json['thumbnail'],
+      price: (json['price'] as num).toDouble(),
+    );
+  }
+}
+
+class GroceryScreen extends StatefulWidget {
+  const GroceryScreen({super.key});
+
+  @override
+  State<GroceryScreen> createState() => _GroceryScreenState();
+}
+
+class _GroceryScreenState extends State<GroceryScreen> {
+  late Future<List<Product>> futureProducts;
+
+  @override
+  void initState() {
+    super.initState();
+    futureProducts = fetchProducts();
+  }
+
+  Future<List<Product>> fetchProducts() async {
+    final response = await http.get(
+      Uri.parse('https://dummyjson.com/products/category/groceries'),
+    );
+
+    if (response.statusCode == 200) {
+      final decoded = json.decode(response.body);
+      List data = decoded['products'];
+      return data.map((e) => Product.fromJson(e)).toList();
+    } else {
+      throw Exception("Failed to load groceries");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text("Grocery Store"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: FutureBuilder<List<Product>>(
+        future: futureProducts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return const Center(child: Text("Error loading groceries"));
+          }
+
+          final products = snapshot.data!;
+
+          return LayoutBuilder(
+            builder: (context, constraints) {
+              int crossAxisCount = 2;
+
+              if (constraints.maxWidth > 1200) {
+                crossAxisCount = 5;
+              } else if (constraints.maxWidth > 900) {
+                crossAxisCount = 4;
+              } else if (constraints.maxWidth > 600) {
+                crossAxisCount = 3;
+              }
+
+              return GridView.builder(
+                padding: const EdgeInsets.all(20),
+                itemCount: products.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: crossAxisCount,
+                  childAspectRatio: 0.8,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                ),
+                itemBuilder: (context, index) {
+                  final item = products[index];
+
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade300,
+                          blurRadius: 8,
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+
+                          
+                          Expanded(
+                            flex: 6,
+                            child: Image.network(
+                              item.image,
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+
+                          const SizedBox(height: 12),
+
+                          
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                              item.title,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+
+                          
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              "\$${item.price.toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.green,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
